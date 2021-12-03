61982cdb4c652cd544f135dc8d54fc7b2b3c416d
EDUARDO SANTOS NASCIMENTO <21276323@CFP127ALUNO.LOCAL>
2021-12-03 16:51:30 -0300

    Bunitin

--- src/br/senai/sp/jandira/ui/Frame.java
+++ src/br/senai/sp/jandira/ui/Frame.java
@@ -16,29 +16,35 @@ import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
+import java.awt.Color;
 
 public class Frame extends JFrame {
 
 	private JPanel contentPane;
 
 	public Frame() {
+		setBackground(new Color(0, 51, 0));
 		
 		ArrayList<String> employees = new ArrayList<>();
 		String caminho = "C:\\Users\\21276323\\Desktop\\jorge\\jorge.txt";
 		Arquivo arquivo = new Arquivo();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 747, 300);
+		setBounds(100, 100, 655, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblManipulacao = new JLabel("Manipula\u00E7\u00E3o de Arquivos");
-		lblManipulacao.setBounds(10, 11, 141, 14);
+		lblManipulacao.setForeground(Color.WHITE);
+		lblManipulacao.setBackground(Color.WHITE);
+		lblManipulacao.setBounds(10, 11, 223, 14);
 		contentPane.add(lblManipulacao);
 		
-		JLabel lblTexto = new JLabel("texto");
+		JLabel lblTexto = new JLabel("Texto:");
+		lblTexto.setForeground(Color.WHITE);
 		lblTexto.setBounds(10, 36, 35, 14);
 		contentPane.add(lblTexto);
 		
@@ -47,15 +53,20 @@ public class Frame extends JFrame {
 		contentPane.add(txtDigitar);
 		
 		JButton btnGravar = new JButton("Gravar");
-		btnGravar.setBounds(243, 61, 150, 50);
+		btnGravar.setForeground(new Color(0, 0, 255));
+		btnGravar.setBackground(new Color(0, 0, 51));
+		btnGravar.setBounds(243, 58, 150, 50);
 		contentPane.add(btnGravar);
 		
 		JButton btnSair = new JButton("Sair");
-		btnSair.setBounds(243, 183, 150, 50);
+		btnSair.setForeground(new Color(255, 255, 0));
+		btnSair.setBackground(new Color(51, 51, 0));
+		btnSair.setBounds(243, 200, 150, 50);
 		contentPane.add(btnSair);
 		
 		JLabel lblArquivo = new JLabel("Texto no Arquivo:");
-		lblArquivo.setBounds(399, 36, 96, 14);
+		lblArquivo.setForeground(Color.WHITE);
+		lblArquivo.setBounds(409, 36, 141, 14);
 		contentPane.add(lblArquivo);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -67,7 +78,9 @@ public class Frame extends JFrame {
 		scrollPane.setViewportView(txtMostrar);
 		
 		JButton btnApagar = new JButton("Apagar");
-		btnApagar.setBounds(243, 122, 150, 50);
+		btnApagar.setForeground(new Color(255, 0, 0));
+		btnApagar.setBackground(new Color(51, 0, 0));
+		btnApagar.setBounds(243, 129, 150, 50);
 		contentPane.add(btnApagar);
 		
 		btnGravar.addActionListener(new ActionListener() {
@@ -104,6 +117,8 @@ public class Frame extends JFrame {
 				
 			}
 		});
+		
+		
 		
 	}
 }
